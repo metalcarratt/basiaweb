ae0af235fe14553dae905bdf327ff15cbd03f77c
Caleb Garratt <calebg@datacom.co.nz>
2020-11-06 15:22:07 +1300

    use hash history for static site

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         { path: "/", redirect: "/home.html" },
         { path: "/home.html", component: () => import("@/pages/Home.vue") },
