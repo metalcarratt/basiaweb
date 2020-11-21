46385a1c981e390fee4e8b4ee94a7c3f5237598f
Caleb Garratt <calebg@datacom.co.nz>
2020-11-22 09:04:06 +1300

    fix mailto

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -38,7 +38,7 @@
             <p>
                 Basia Garratt<br/>
                 Hamilton, New Zealand<br/>
-                Email: <a href="mailto=basgarratt@gmail.com">basgarratt@gmail.com</a><br/>
+                Email: <a href="mailto:basgarratt@gmail.com">basgarratt@gmail.com</a><br/>
                 Phone: +64 22 415 2822 / 022 415 2822
             </p>
         </div>
